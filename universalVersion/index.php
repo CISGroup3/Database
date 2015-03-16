b5b9e3f0c00fcd21cc7e766ecd16094c39ca3424
Oza1994 <oza_rock@hotmail.com>
2015-03-16 11:40:17 +0000

    Universal Version 1.13 - Owen
    Added sign in/Register page
    Added background
    Added terms and conditions

--- universalVersion/index.php
+++ universalVersion/index.php
@@ -4,30 +4,26 @@ session_start(); //starts the session to store certain variables using cookies
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
 <title>Home</title>
 <meta charset = "utf-8">
-
 <link rel="stylesheet" type="text/css" href="CSS/mystyle.css"/>
 
-
 <script type="text/javascript" src="Javascript/Google.js"></script>
-<script src="Javascript/newsfeed.js"
-type="text/javascript"></script>
+<script src="Javascript/newsfeed.js" type="text/javascript"></script>
 
 <div id = "newsfeedcss">
-<style type="text/css">
-@import url("CSS/newsfeedstyle.css");
-</style>
-
+	<style type="text/css">
+		@import url("CSS/newsfeedstyle.css");
+	</style>
 </div>
-<script type="text/javascript">
-function load() {
-var feed ="http://feeds.feedburner.com/Securityweek?format=xml";
-new GFdynamicFeedControl(feed, "feedControl");
 
-}
+<script type="text/javascript">
+function load() 
+	{
+		var feed ="http://feeds.feedburner.com/Securityweek?format=xml";
+		new GFdynamicFeedControl(feed, "feedControl");
+	}
 google.load("feeds", "1");
 google.setOnLoadCallback(load);
 </script>
@@ -36,9 +32,8 @@ google.setOnLoadCallback(load);
 <body>
 
 <?php
-		$loggedIn = "false";
-		
-		if (array_key_exists('userEmail', $_SESSION) && !empty($_SESSION['userEmail'])) 
+	$loggedIn = "false";
+	if (array_key_exists('userEmail', $_SESSION) && !empty($_SESSION['userEmail'])) 
 			{
 				$loggedIn = "true";
 			}
@@ -57,14 +52,14 @@ google.setOnLoadCallback(load);
 					<nav>
 						<ul>
 							<?php //if user is logged in, welcome them by user forum name
-						if($loggedIn == "true" && !empty($_SESSION['userNickname']))
-						{
-						   echo "<li> <b> Welcome, " .$_SESSION['userNickname'] ."!</li> </b>"; 
-						}
-						if($loggedIn == "false")
-						{
-							echo "<li><b></li></b>";
-						}
+								if($loggedIn == "true" && !empty($_SESSION['userNickname']))
+								{
+								echo "<li> <b> Welcome, " .$_SESSION['userNickname'] ."!</li> </b>"; 
+								}
+								if($loggedIn == "false")
+								{
+								echo "<li><b></li></b>";
+								}
 							?>
 						</ul>
 					</nav>
@@ -73,21 +68,18 @@ google.setOnLoadCallback(load);
 				<div id = "nav3">
 					<nav>
 						<ul>
-
-							<li><a href="registryTest.php">Register</a></li>
+							<li>
+							<a href="registryTest.php">Register</a></li>
 							<li><b>|</b></li>
+							
 							<?php
 							if ($loggedIn == "false")
-							
 							echo "<li><a href=loginTest.php>Sign in</a></li>"
-							
 							?>
+							
 							<?php
 							if ($loggedIn == "true")
-							
 							echo "<li><a href=logout.php>Sign out</a></li>"
-							
-							
 							?>
 						</ul>
 					</nav>
@@ -97,46 +89,44 @@ google.setOnLoadCallback(load);
 	</div>
 	
 	<div id="wrapper">
-	<div id="homepic">
+		<div id="homepic">
 			<img src="Images/athenasecuritylogo.png" alt="Athena Security" title="Athena Security" height="150" width="350"/>
-			</div>
-
-		<p>
-			Paragraph explaining who we are, the purpose of this website and why this website was made 
-			(Use research to back up the creation of this website ie. News) Also make it interesting as
-			possible, it will be the first thing the user will see so its important that it gets their
-			attention and curiosity.
+		</div>
+			<p>
+				Paragraph explaining who we are, the purpose of this website and why this website was made 
+				(Use research to back up the creation of this website ie. News) Also make it interesting as
+				possible, it will be the first thing the user will see so its important that it gets their
+				attention and curiosity.
 			
-			<br><br>
+				<br><br>
 	
-			Short summary of what this website has to offer. (NOTE: Make sure there's enough content
-			so that the footer is at the bottom of the page (no white space at the bottom)
-		</p>
+				Short summary of what this website has to offer. (NOTE: Make sure there's enough content
+				so that the footer is at the bottom of the page (no white space at the bottom)
+			</p>
 	</div>
 	
 <div id="feedControl">Loading...</div>
 
 	<div id="footer">
-			<ul>
-				<li><a href="index.php">Home</a></li>
-				<li><b>|</b></li>
-				<li><a href="anti-virus.php">Anti-Virus</a></li>
-				<li><b>|</b></li>
-				<li><a href="FAQ.php">FAQ</a></li>
-				<li><b>|</b></li>
-				<li><a href="forumMenu.php">Forum</a></li>
-				<li><b>|</b></li>
-				<li><a href="Trouble-shooting.html">Trouble-Shooting</a></li>	
-			</ul>
+		<ul>
+			<li><a href="index.php">Home</a></li>
+			<li><b>|</b></li>
+			<li><a href="anti-virus.php">Anti-Virus</a></li>
+			<li><b>|</b></li>
+			<li><a href="FAQ.php">FAQ</a></li>
+			<li><b>|</b></li>
+			<li><a href="forumMenu.php">Forum</a></li>
+			<li><b>|</b></li>
+			<li><a href="Trouble-shooting.html">Trouble-Shooting</a></li>	
+		</ul>
 	</div>
-	    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="Javascript/1.11.1.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="Javascript/3.2.0.js"></script>
-
-        <script>
-            $('.help').tooltip()
-
-        </script>
+    <script>
+	$('.help').tooltip()
+	</script>
 </body>
 </html>
--- universalVersion/loginTest.php
+++ universalVersion/loginTest.php
@@ -1,30 +1,147 @@
-<!DOCTYPE html>
+<?php
+session_start(); //starts the session to store certain variables using cookies
+ ?>
 
 <head>
-<title> Registry Test </title>
+<title>Register</title>
+<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+<link rel="stylesheet" type="text/css" href="CSS/mystyle.css"/>
+<!DOCTYPE html>
+<html lang="en">
+<meta charset = "utf-8">
+<link rel="stylesheet" type="text/css" href="CSS/mystyle.css"/>
 </head>
 
+<div id = "b2">
+<?php
+		$loggedIn = "false";
+		
+		if (array_key_exists('userEmail', $_SESSION) && !empty($_SESSION['userEmail'])) 
+			{
+				$loggedIn = "true";
+			}
+?>
+	<div id = "nav">
+		<nav>
+			<ul>
+				<li><img src="Images/athenalogo.png" alt="Athena Security" title="Athena Security" height="50" width="65"></li>
+				<li><a href="index.php">Home</a></li>
+				<li><a href="anti-virus.php">Anti-Virus</a></li>
+				<li><a href="FAQ.php">FAQ</a></li>
+				<li><a href="forumMenu.php">Forum</a></li>
+				<li><a href="Trouble-shooting.html">Trouble-Shooting</a></li>
+				
+				<div id = "nav2">
+					<nav>
+						<ul>
+							<?php //if user is logged in, welcome them by user forum name
+						if($loggedIn == "true" && !empty($_SESSION['userNickname']))
+						{
+						   echo "<li> <b> Welcome, " .$_SESSION['userNickname'] ."!</li> </b>"; 
+						}
+						if($loggedIn == "false")
+						{
+							echo "<li><b></li></b>";
+						}
+							?>
+						</ul>
+					</nav>
+				</div>
+				
+				<div id = "nav3">
+					<nav>
+						<ul>
+							<li><a href="registryTest.php">Register</a></li>
+							<li><b>|</b></li>
+							<?php
+							if ($loggedIn == "false")
+							
+							echo "<li><a href=loginTest.php>Sign in</a></li>"
+							
+							?>
+							<?php
+							if ($loggedIn == "true")
+							
+							echo "<li><a href=logout.php>Sign out</a></li>"
+							?>
+						</ul>
+					</nav>
+				</div>
+			</ul>	
+		</nav>
+	</div>
+
+	
+<div id = "borderimg">
+<img src="Images/container.png" width="860" height="190">
+</div>
+	<div id = "container">
+	
 <?php if(isset($_GET['login']));/*user wants to login link*/?> 
 
 		
 		
 		<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
 			Email Address:<br>
-			<input type="text" name="userEmail">
+			<input type="text" name="userEmail" size="50">
 			<br>
 
 		<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
 			Password:<br>
-			<input type="password" name="userPassword">
-			<br>
+			<input type="password" name="userPassword" size="50">
+			<br><br>
 
-		<input type="submit" value="SUBMIT" /> 
+		<input type="submit" value="Sign in"/> 
 				
-		</form> 
+		</form>
+		</div>
+
+		<div id = "heading">
+		Terms and Conditions
+		</div>
+		
+		<p>
+		<ul>
+		<li>Do not submit comments that contain personal information.</li>
+		<li>Do not submit comments that are unlawful, harassing, abusive, threatening, harmful, obscene, profane, sexually orientated or racially offensive.</li>
+		<li>Do not swear or use language that could offend other forum participants.</li>
+		<li>Do not advertise or promote products or services.</li>
+		<li>Do not spam or flood the forum. Only submit a comment once.</li>
+		<li>Do not resubmit the same, or similar, comments.</li>
+		<li>Keep your comments relevant to the discussion topic.</li>
+		<li>Do not submit defamatory comments (comments that are untrue and capable of damaging the reputation of a person or organisation).</li>
+		<li>Do not condone illegal activity or incite people to commit any crime, including incitement of racial hatred.</li>
+		<li>Do not submit comments that could prejudice on-going or forthcoming court proceedings (contempt of court) or break a court injunction.</li>
+		<li>Do not submit comments containing someone else's copyright material.</li>
+		<li>Do not swear or use language that could offend other forum participants.</li>
+		<li>Do not otherwise submit comments that are unlawful, harassing, abusive, threatening, harmful, obscene, profane, sexually orientated or racially offensive. This includes comments that are offensive to others with regards to religion, gender, nationality or other personal characteristic.</li>
+		<li>Do not impersonate other forum members or falsely claim to represent a person or organisation.</li>
+		<li>Do not submit comments or choose user names that contain personal information that would identify yourself or others. For example last names, addresses, phone numbers, email addresses or other online contact details either relating to yourself or other individuals.</li>
+		<li>Do not post comments in languages other than English. </li>
+		<li>Do not advertise or promote products or services. </li>
+		<li>Do not spam or flood the forum. Only submit a comment once. Do not resubmit the same, or similar, comments. Keep the number of comments you submit on a topic at a reasonable level. Multiple comments from the same individual, or a small number of individuals, may discourage others from contributing. </li>
+		<li>Do not use an inappropriate user name (vulgar, offensive etc.).</li>
+		<li>If you are under the age of 12 please get your parent/guardian's permission before participating in this forum. Users without this consent are not allowed to participate or provide us with personal information.</li>
+		</ul>
+		</p>
+		
+		<div id="footer">
+		<ul>
+			<li><a href="index.php">Home</a></li>
+			<li><b>|</b></li>
+			<li><a href="anti-virus.php">Anti-Virus</a></li>
+			<li><b>|</b></li>
+			<li><a href="FAQ.php">FAQ</a></li>
+			<li><b>|</b></li>
+			<li><a href="forumMenu.php">Forum</a></li>
+			<li><b>|</b></li>
+			<li><a href="Trouble-shooting.html">Trouble-Shooting</a></li>	
+		</ul>
+	</div>
 
 <?php //default
 //connect to the db 
-session_start();
+//session_start(); DO WE NEED THIS
 
 
 $dbcnx = @mysql_connect('localhost', 'root', 'cisgroup');
@@ -93,5 +210,5 @@ $dbcnx = @mysql_connect('localhost', 'root', 'cisgroup');
 		}
 }
 ?>
-
+</div>
 </html>
