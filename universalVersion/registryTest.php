3bc44e78ce52b7cd316fc7f3e019acec6db22bcf
Ciel21 <katrionaangel@rocketmail.com>
2015-03-18 15:10:40 +0000

    UV 1.16
    Fix login/registry error.

--- universalVersion/registryTest.php
+++ universalVersion/registryTest.php
@@ -237,7 +237,7 @@ $dbcnx = @mysql_connect('localhost', 'root', 'cisgroup');
 						$_SESSION['userEmail'] = $x; //specifically ensure that the system does not auto login the user after they are registered. 
 						session_unset(); //unset the session variables 
 						echo '<META HTTP-EQUIV="Refresh" Content="0; URL=index.php">'; //redirects to prevent the user refreshing the page and creating a user account twice
-					}
+					} //end if
 				
 				else if ($sql === false) //if the query doesn't work/returns false, then catch the error gracefully
 					{
